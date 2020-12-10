59d1efd8dea79ef91e1e4be59cbe2a2a5e09dbea
gqadirdepar <38527806+gqadirdepar@users.noreply.github.com>
2020-12-10 23:18:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div >
-     This is my first web of hello word Ghulam Qadir
+     This is my first web of hello word GhulamQadirDepar
     </div>
   );
 }
