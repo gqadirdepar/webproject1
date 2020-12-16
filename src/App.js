0d73d5402a4b496fb9f891bdb3f05784ac28de7b
gqadirdepar <gqadirdepar@gmail.com>
2020-12-16 16:04:37 +0500

    updated from office pc

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div >
-     This is my first web of hello word Ghulam Qadir Depar S/o Abdul Razak
-    
-    This is my website
+     updated from office by Ghulam Qadir 
     </div>
   );
 }
